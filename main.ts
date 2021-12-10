f0b7648e335056a32a30c71f8c42ec6d86605ba8
GerbH57 <92798943+GerbH57@users.noreply.github.com>
2021-12-10 08:53:44 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
+// Sets radio to 1
 let pressure_sensor = 0
 radio.setGroup(1)
 basic.forever(function () {
     pressure_sensor = pins.analogReadPin(AnalogPin.P0)
     pins.setAudioPin(AnalogPin.P1)
+    // Asks if the pressure sensor is pressed
     if (pressure_sensor > 0) {
-        radio.sendNumber(1)
-        for (let index = 0; index < 50000; index++) {
+        radio.sendString("Burglar")
+        for (let index = 0; index < 1e+308; index++) {
             music.playMelody("C5 C5 C C C C5 C5 C5 ", 9999999999999)
         }
     }
